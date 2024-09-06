d72c5b4a2b59a0fcdd1921924a7a0c612c135604
AmongUs <128269328+tanhoangviet@users.noreply.github.com>
2024-09-06 19:12:09 +0700

    Update AmongUs-X-GiangMc-Hub.lua

--- AmongUs-X-GiangMc-Hub.lua
+++ AmongUs-X-GiangMc-Hub.lua
@@ -12,7 +12,7 @@ end
 repeat wait() until game.Players.LocalPlayer
 wait(0.25)
 _G.AutoExecuter = true
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Playgiang/Slap_Battles/main/Slap_Battles.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tanhoangviet/Script-Slap-Battles/main/AmongUs-X-GiangMc-Hub.lua"))()
     ]])
 else
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "Error",Text = "You cannot auto execute",Icon = "rbxassetid://7733658504",Duration = 5})
